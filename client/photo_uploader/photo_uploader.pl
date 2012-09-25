7e692d60398ffa3ea74954f8a7bab2bb5ea8923b
@equinox79 <junichiro79@gmail.com>
2012-09-26 01:00:06 +0900

    README.pod追加

--- client/photo_uploader/photo_uploader.pl
+++ client/photo_uploader/photo_uploader.pl
@@ -3,19 +3,17 @@
 use strict;
 use warnings;
 use utf8;
-use lib qw| lib ../../lib |;
+use lib qw(./lib ../../lib);
 use Yancha::Client::PhotoUploader;
 
 my $uploader = Yancha::Client::PhotoUploader->new(
-    #yancha_url   => 'http://localhost:3000',
-    #pyazo_url    => 'http://localhost:5000',
     yancha_url   => 'http://yancha.hachiojipm.org:443',
-    pyazo_url    => 'http://yairc.cfe.jp:5000',
+    pyazo_url    => 'http://yairc.cfe.jp',
     api_endpoint => '/api/post',
-    upload_path  => '/tmp/upload',
+    upload_path  => '/tmp/Eye-Fi',
     nick         => 'PhotoUploader',
-    tag          => '#LTTHON',
-    interval     => 1,
+    tag          => '#PUBLIC',
+    interval     => 3,
 );
 
 $uploader->run;
